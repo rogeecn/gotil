f73acd14ddd4953142b8330c065664806c3953c8
rogeecn <rogeecn@gmail.com>
2017-12-07 15:15:01 +0800

    init project

--- str/str.go
+++ str/str.go
@@ -12,3 +12,10 @@ func Format(format string, m map[string]interface{}) string {
 	return format
 }
 
+func Reverse(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
